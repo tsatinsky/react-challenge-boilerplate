0fdaffd774d5ce21c81ba291026d0becaca4f380
Tomas Satinsky <tomas.satinsky@gooddata.com>
2022-03-10 09:53:50 +0100

    improve payment detail style

---
+++ src/components/shared/ThreeDSResponseBadge.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { PaymentStatus } from "../../lib/payment";
+import Badge, { BadgeProps } from "../design/Badge";
+
+export interface PaymentStatusBadgeProps {
+    status: PaymentStatus;
+    large?: boolean;
+}
+
+const PaymentStatusBadge: React.FC<PaymentStatusBadgeProps> = ({ status, large }) => {
+    const text = status.toLowerCase();
+    let badgeProps: BadgeProps = {};
+    if (status === "AUTHORIZED" || status === "SETTLED") {
+        badgeProps = { success: true };
+    } else if (status === "FAILED" || status === "CANCELLED") {
+        badgeProps = { error: true };
+    }
+    return (
+        <Badge {...badgeProps} large={large}>
+            {text}
+        </Badge>
+    );
+};
+
+export default PaymentStatusBadge;
